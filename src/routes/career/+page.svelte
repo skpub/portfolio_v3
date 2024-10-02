2e276d1cb1eb563590595bd53c527870b9f737d4
skpub <satodeyannsu@gmail.com>
2024-10-02 12:02:27 +0900

    履歴の項目をちょっと追加

--- src/routes/career/+page.svelte
+++ src/routes/career/+page.svelte
@@ -1,1 +1,28 @@
 <h1>履歴</h1>
+
+<h2 class="title">2017/04 一関工業高等専門学校 入学 (未来創造工学科改組後一期生)</h2>
+<div class="box">
+  <div class="vertical_line"></div>
+  <div class="content">てすと</div>
+</div>
+<h3 class="title">2018/04 情報・ソフトウェア系 配属</h3>
+<div class="box">
+  <div class="vertical_line"></div>
+  <div class="content">てすと</div>
+</div>
+<h2 class="title">2022/03 一関工業高等専門学校 卒業</h2>
+<style>
+  .box {
+    display: flex;
+  }
+  .vertical_line {
+    width: 24px;
+    border-right: 1px solid var(--foreground);
+  }
+  .content {
+    margin: 12px;
+  }
+  .title {
+    margin: 0;
+  }
+</style>
