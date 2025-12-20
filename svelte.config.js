3b7fa15d23d17c772f81aa5ab8c8c290ad14e7cc
skpub <satodeyannsu@gmail.com>
2025-12-21 01:36:24 +0900

    CF

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-cloudflare";
+
 // import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,18 +9,20 @@ const config = {
 	// preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			preprocess: false,
-			strict: true
-		})
-	}
+			config: undefined,
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined,
+			},
+			fallback: "plaintext",
+			routes: {
+				include: ["/*"],
+				exclude: ["<all>"],
+			},
+		}),
+	},
 };
 
 export default config;
-
